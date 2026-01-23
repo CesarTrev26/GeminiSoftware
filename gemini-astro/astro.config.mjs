b4e9806e0713526978baa52980f5020ab73716ac
Cesar <cesarjtrevino@gmail.com>
2026-01-23 16:57:31 -0600

    Fix astro issues

--- gemini-astro/astro.config.mjs
+++ gemini-astro/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import compress from 'astro-compress';
 
 // https://astro.build/config
